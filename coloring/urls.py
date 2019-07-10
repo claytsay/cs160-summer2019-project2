d92bf272c6681e7c8606d6a98a61688ae075dbb0
clay.tsay <clay.tsay@gmail.com>
2019-07-10 18:44:32 -0400

    Reorganized Files

--- coloring/urls.py
+++ coloring/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('', views.colour_palette, name='colour_palette')
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'coloring/index.html')
+  
+def colour_palette(request):
+    return render(request, 'coloring/colour-palette.html')
